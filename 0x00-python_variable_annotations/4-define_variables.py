530b10739c3d4e72da413c8ffee77239eed55935
okeomasilchi <okeomasilachi@gmail.com>
2024-03-14 23:26:26 +0100

    Add variable definitions with type annotations

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""4. Define variables"""
+
+
+# Define variables with type annotations
+a: int
+pi: float
+i_understand_annotations: bool
+school: str
+a = 3
+pi = 3.14
+i_understand_annotations = True
+school = "Holberton"
