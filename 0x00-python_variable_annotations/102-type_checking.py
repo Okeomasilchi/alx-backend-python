0ecc9967a8e13b46182a25b0eabae5e88c250c23
okeomasilchi <okeomasilachi@gmail.com>
2024-03-15 00:10:42 +0100

    Add type annotations and update function to zoom in on elements in a tuple

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,1 +1,23 @@
-#!/usr/bin/env python3
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+    """Zooms in on each element in the input tuple by repeating it 'factor' times.
+
+    Args:
+        lst (Tuple): The input tuple containing elements to be zoomed in on.
+        factor (int, optional): The factor by which each element should be zoomed in.
+                                Defaults to 2.
+
+    Returns:
+        Tuple: A new tuple containing the zoomed-in elements.
+    """
+    zoomed_in: List = [item for item in lst for i in range(factor)]
+    return tuple(zoomed_in)
+
+
+array = (12, 72, 91)  # Use a tuple instead of a list
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)  # Provide an integer value for the factor parameter
