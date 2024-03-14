f4dbd7742ce0349f1ac0b06f74c45f8169359392
okeomasilchi <okeomasilachi@gmail.com>
2024-03-14 23:07:15 +0100

    Refactor code for improved performance

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" 0. Basic annotations - add """
+
+
+def add(a: float, b: float) -> float:
+    """
+    The function `add` takes two float numbers as input
+    and returns their sum as a float.
+
+    Args:
+      a (float): float
+      b (float): The parameters `a` and `b` are both of type
+        `float`, and the function `add` takes two float numbers
+        `a` and `b` as input and returns their sum as a float.
+
+    Returns:
+      the sum of the two input parameters `a` and `b`, which are
+      both floats.
+    """
+    return a + b
