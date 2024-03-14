0e06ea7d22a42d1ddcc53608c8b3c5e4901489b3
okeomasilchi <okeomasilachi@gmail.com>
2024-03-15 00:14:28 +0100

    Refactor zoom_array function to improve readability and add missing type annotations

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ 102. Type Checking """
 
+
 from typing import Tuple, List
 
 
@@ -10,12 +11,14 @@ def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
     repeating it 'factor' times.
 
     Args:
-        lst (Tuple): containing elements to be zoomed in on.
-        factor (int, optional): The factor by which each element
-            should be zoomed in. Defaults to 2.
+        lst (Tuple): containing elements to be zoomed in
+            on.
+        factor (int, optional): The factor by which each
+            element should be zoomed in. Defaults to 2.
 
     Returns:
-        Tuple: A new tuple containing the zoomed-in elements.
+        Tuple: A new tuple containing the zoomed-in
+        elements.
     """
     zoomed_in: List = [item for item in lst for i in range(factor)]
     return tuple(zoomed_in)
@@ -25,4 +28,5 @@ array = (12, 72, 91)  # Use a tuple instead of a list
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3)  # Provide an integer value for the factor parameter
+# Provide an integer value for the factor parameter
+zoom_3x = zoom_array(array, 3)
