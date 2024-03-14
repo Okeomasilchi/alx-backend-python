26a9a3bb1a9fd21baf2229c12d9fdfbc16f0167f
okeomasilchi <okeomasilachi@gmail.com>
2024-03-14 23:35:53 +0100

    Add sum_list function to calculate the sum of a list of floats

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+""" complex types - list of floats """
+
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """returns the sum of the list of floats"""
+    return sum(input_list)
