46b91b62bee67438151b930910e911715fa633c7
okeomasilchi <okeomasilachi@gmail.com>
2024-03-14 23:10:30 +0100

    Add type annotations and docstring to the concat function

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+""" 1. Basic annotations - concat """
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    The function `concat` takes two string parameters
+    `str1` and `str2` and returns their concatenated
+    string as a string.
+
+    Args:
+      str1 (str): string
+      str2 (str): string
+
+    Returns:
+      the concatenated string of the two input parameters
+      `str1` and `str2`,which are both strings.
+    """
+    return str1 + str2
