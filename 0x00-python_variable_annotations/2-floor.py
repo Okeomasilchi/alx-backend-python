003fb3aeefbd3b54cd52e8c85b3e43671acb674c
okeomasilchi <okeomasilachi@gmail.com>
2024-03-14 23:22:25 +0100

    Add floor function with type annotations and test cases

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+""" 2. Basic annotations - floor """
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    The function `floor` takes a float `n` as argument
+    and returns the floor of the float `n` as an int.
+
+    Args:
+      n (float): float
+
+    Returns:
+      the floor of the float `n` as an int.
+    """
+    return math.floor(n)
