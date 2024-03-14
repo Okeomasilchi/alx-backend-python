81feda6bbd526e06a2bdf6204007ed855e1cdb11
okeomasilchi <okeomasilachi@gmail.com>
2024-03-14 23:28:09 +0100

    Update variable definitions with type annotations

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -3,11 +3,7 @@
 
 
 # Define variables with type annotations
-a: int
-pi: float
-i_understand_annotations: bool
-school: str
-a = 3
-pi = 3.14
-i_understand_annotations = True
-school = "Holberton"
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
