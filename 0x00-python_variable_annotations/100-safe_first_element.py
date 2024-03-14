8c212507e3171dca3d40e6aca196370d3be99da1
okeomasilchi <okeomasilachi@gmail.com>
2024-03-15 00:02:01 +0100

    Add safe_first_element function and update main.py

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+""" 100. Safe first element! """
+
+from typing import Sequence, Union, Any
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Returns the first element of a list."""
+    if lst:
+        return lst[0]
+    else:
+        return None
