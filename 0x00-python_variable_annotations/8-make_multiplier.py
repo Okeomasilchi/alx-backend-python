e6ddaafbbaac64be3d8628ab3bb6d905a66e98ae
okeomasilchi <okeomasilachi@gmail.com>
2024-03-14 23:46:43 +0100

    Add make_multiplier function to main.py

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+""" complex types - list of floats """
+
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    returns a function that multiplies a float by multiplier
+    """
+
+    def f(n: float) -> float:
+        """
+        returns a float
+        """
+        return n * multiplier
+
+    return f
