cea263509d8da13612c5c7c40f6eccf7b34b194b
okeomasilchi <okeomasilachi@gmail.com>
2024-03-14 23:51:46 +0100

    Add element_length function to calculate length of sequences

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+"""9. Let's duck type an iterable object"""
+
+
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    The function `element_length` takes an iterable of
+    sequences and returns a list of tuples where each
+    tuple contains a sequence from the input along with
+    its length.
+
+    Args:
+      lst (Iterable[Sequence]): iterable containing sequences.
+
+    Returns:
+      list of tuples.
+    """
+    return [(i, len(i)) for i in lst]
