58cbeca8e2fd748f06de194151871e2b7cd72216
okeomasilchi <okeomasilachi@gmail.com>
2024-03-19 00:45:23 +0100

    Take the code from wait_n and alter it into a new function task_wait_n. The code is nearly identical to wait_n except task_wait_random is being called.

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,19 +7,24 @@ import asyncio
 from typing import List
 
 
-task_wait_random = __import__("3-tasks").task_wait_random
+task_wait_random = __import__("3-tasks"
+                              ).task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List[float]:
-  """
-  Executes multiple instances of the task_wait_random coroutine concurrently.
-  
-  Args:
-    n (int): The number of tasks to execute.
-    max_delay (int): The maximum delay for each task.
-  
-  Returns:
-    List[float]: A list of floats representing the delays of each task.
-  """
-  tasks = [task_wait_random(max_delay) for _ in range(n)]
-  return await asyncio.gather(*tasks)
+async def task_wait_n(n: int,
+                      max_delay: int
+                      ) -> List[float]:
+    """
+    Executes multiple instances of the task_wait_random
+    coroutine concurrently.
+
+    Args:
+      n (int): The number of tasks to execute.
+      max_delay (int): The maximum delay for each task.
+
+    Returns:
+      List[float]: A list of floats representing the
+      delays of each task.
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    return await asyncio.gather(*tasks)
