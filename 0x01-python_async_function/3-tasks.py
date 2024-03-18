efa461be3d6c171ff6949e65ecb0148ce39a5728
okeomasilchi <okeomasilachi@gmail.com>
2024-03-19 00:36:33 +0100

    Import wait_random from 0-basic_async_syntax.
    Write a function (do not create an async function, use the regular function syntax to do this) task_wait_random that takes an integer max_delay and returns a asyncio.Task.

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,1 +1,29 @@
 #!/usr/bin/env python3
+"""
+module that contains the function task_wait_random
+"""
+
+import asyncio
+from asyncio import Task
+from typing import TypeVar
+
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+_T = TypeVar("_T")
+
+
+def task_wait_random(max_delay: int) -> Task[_T]:
+    """
+    Create and return a task that waits for a random
+    amount of time.
+
+    Args:
+      max_delay (int): The maximum delay in seconds.
+
+    Returns:
+      Task: A task that represents the asynchronous
+      operation of waiting for a random amount of time.
+    """
+    return asyncio.create_task(wait_random(max_delay))
