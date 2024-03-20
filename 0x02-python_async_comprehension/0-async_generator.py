f3a9cca0da1f7a22177d50fd0a8cf1776b8e3953
okeomasilchi <okeomasilachi@gmail.com>
2024-03-20 01:03:21 +0100

    Add async generator and print yielded values

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+
+"""
+0. Async Generator
+"""
+
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    The async_generator function asynchronously
+    yields random numbers between 0 and 10 after
+    a 1-second delay for each iteration.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
