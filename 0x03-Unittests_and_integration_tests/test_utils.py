2265f8494e151adabc2a901c1214c50a833f4e34
okeomasilchi <okeomasilachi@gmail.com>
2024-04-08 15:46:01 +0100

    Refactor TestAccessNestedMap.test_access_nested_map_exception method to use a more descriptive variable name for expected_error

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,6 @@
 """
 Parameterize a unit test
 """
-
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
@@ -12,26 +11,21 @@ class TestAccessNestedMap(unittest.TestCase):
     """
     Test case class for testing the access_nested_map function.
     """
-    @parameterized.expand(
-        [
-            ({"a": 1}, ("a",), 1),
-            ({"a": {"b": 2}}, ("a",), {"b": 2}),
-            ({"a": {"b": 2}}, ("a", "b"), 2),
-        ]
-    )
-    def test_access_nested_map(self, nested_map, path, expected_result):
+    @parameterized.expand([({}, ("a",), "a"), ({"a": 1}, ("a", "b"), "b")])
+    def test_access_nested_map_exception(
+      self,
+      nested_map,
+      path,
+      expected_error
+      ):
         """
-        Test the access_nested_map function with different inputs.
-
-        Args:
-          nested_map (dict): The nested map to be accessed.
-          path (tuple): The path to the desired value in the nested map.
-          expected_result: The expected result of accessing the nested map.
-
-        Returns:
-          None
+        Test that a KeyError is raised when accessing a
+        non-existing key in the nested map.
         """
-        self.assertEqual(access_nested_map(nested_map, path), expected_result)
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+
+        self.assertEqual(str(context.exception), f"'{expected_error}'")
 
 
 if __name__ == "__main__":
