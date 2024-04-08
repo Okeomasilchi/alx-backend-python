6f7dda5723d5791a264ccd0336c8395aa4179069
okeomasilchi <okeomasilachi@gmail.com>
2024-04-08 15:48:09 +0100

    Add parameterized test case for access_nested_map function in test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,6 +2,7 @@
 """
 Parameterize a unit test
 """
+
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
@@ -11,6 +12,28 @@ class TestAccessNestedMap(unittest.TestCase):
     """
     Test case class for testing the access_nested_map function.
     """
+
+    @parameterized.expand(
+        [
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2),
+        ]
+    )
+    def test_access_nested_map(self, nested_map, path, expected_result):
+        """
+        Test the access_nested_map function with different inputs.
+
+        Args:
+          nested_map (dict): The nested map to be accessed.
+          path (tuple): The path to the desired value in the nested map.
+          expected_result: The expected result of accessing the nested map.
+
+        Returns:
+          None
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
     @parameterized.expand([({}, ("a",), "a"), ({"a": 1}, ("a", "b"), "b")])
     def test_access_nested_map_exception(
       self,
